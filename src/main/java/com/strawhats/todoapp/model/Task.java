2c156a20e22e36524cb1102913bda382322b1e00
lrdluffy <lrdaftkhar@gmail.com>
2025-07-03 10:19:53 +0330

    Created the entity

---
+++ src/main/java/com/strawhats/todoapp/model/Task.java
@@ -0,0 +1,24 @@
+package com.strawhats.todoapp.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDate dueDate;
+    private LocalDateTime createdAt;
+
+}
